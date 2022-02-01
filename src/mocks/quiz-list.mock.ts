03ff800ab9f0ea3d51b35adf2d5492af428419fb
Ludovic <lulubcancer@hotmail.fr>
2022-02-01 10:55:42 +0100

    1ere partie du td jusqu'a l'exercice 6 (inclus)

--- src/mocks/quiz-list.mock.ts
+++ src/mocks/quiz-list.mock.ts
@@ -1,28 +1,47 @@
 import { Quiz } from '../models/quiz.model';
 import { Question } from '../models/question.model';
 
-// export const QUESTION_ACTOR: Question = {
-//     label: 'Jean Gabin a joué dans...',
-//     answers: [
-//         {
-//             value: 'Les tuches II',
-//             isCorrect: false,
-//         },
-//         {
-//             value: 'La grande illusion',
-//             isCorrect: true,
-//         }
-//     ]
-// };
+export const QUESTION_ACTOR: Question = {
+    label: 'Jean Gabin a joué dans...',
+    answers: [
+        {
+            value: 'Les tuches II',
+            isCorrect: false,
+        },
+        {
+            value: 'La grande illusion',
+            isCorrect: true,
+        }
+    ]
+};
+
+export const QUESTION_SPORT: Question = {
+    label: 'Bastien est ...',
+    answers: [
+        {
+            value: 'une pute',
+            isCorrect: false,
+        },
+        {
+            value: 'une tchoin',
+            isCorrect: true,
+        },
+        {
+            value: 'un homme',
+            isCorrect: false,
+        }
+    ]
+};
 
 export const QUIZ_LIST: Quiz[] = [
     {
-        name: 'Les Acteurs', // What's happening if I change this value..?
+        name: 'Bastien', // What's happening if I change this value..?
         theme: 'Actor',
-        questions: [],
+        questions: [QUESTION_ACTOR],
     },
     {
-        name: 'Les Sports',
-        questions: [],
+        name: 'La tchoin',
+        theme: 'Sport',
+        questions: [QUESTION_SPORT],
     }
 ];
--- src/services/quiz.service.ts
+++ src/services/quiz.service.ts
@@ -30,5 +30,12 @@ export class QuizService {
   addQuiz(quiz: Quiz) {
     // You need here to update the list of quiz and then update our observable (Subject) with the new list
     // More info: https://angular.io/tutorial/toh-pt6#the-searchterms-rxjs-subject
+    this.quizzes.push(quiz);
+    this.quizzes$.next(this.quizzes);
+  }
+
+  deleteQuiz(quiz: Quiz){
+    delete this.quizzes[this.quizzes.indexOf(quiz)]
+    this.quizzes$.next(this.quizzes);
   }
 }
